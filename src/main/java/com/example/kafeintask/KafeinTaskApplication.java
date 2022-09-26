4672c240dc045e9ed66e138edf641387f6f58fff
Yunus Emre ÖZTÜRK <50083324+emreyunus61@users.noreply.github.com>
2022-09-26 09:33:08 +0300

    Created project and added dependencies.

---
+++ src/main/java/com/example/kafeintask/KafeinTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.kafeintask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafeinTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafeinTaskApplication.class, args);
+    }
+
+}
